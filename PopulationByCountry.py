67ec4f9c21d75448b5e81435464af355b07ca5af
ThilinaJs <112922490+ThilinaJs@users.noreply.github.com>
2022-11-09 22:36:51 +0530

    Update PopulationByCountry.py

--- PopulationByCountry.py
+++ PopulationByCountry.py
@@ -18,9 +18,16 @@ for row in rows:
     dic['Population']=row.find_all('td')[2].text.replace(',','')
     dic['Yearly Change']=row.find_all('td')[3].text
     dic['Net Change']=row.find_all('td')[4].text.replace(',','')
-    dic['Density (P/SquareKm) ']=row.find_all('td')[5].text
-    dic['Land Area (SquareKm) ']=row.find_all('td')[6].text.replace(',','')
+    dic['Density (P/SquareKm)']=row.find_all('td')[5].text
+    dic['Land Area (SquareKm)']=row.find_all('td')[6].text.replace(',','')
+    dic['Migrants (net)']=row.find_all('td')[7].text.replace(',','')
+    dic['Fert. Rate']=row.find_all('td')[8].text
+    dic['Med. Age']=row.find_all('td')[9].text
+    dic['Urban Pop %']=row.find_all('td')[10].text
+    dic['World Share']=row.find_all('td')[11].text
 
     countries_list.append(dic)
 
-print(countries_list[0])
+dataset = pd.DataFrame(countries_list)
+
+dataset.to_excel("PopulationByCountry.xlsx", index=False)
